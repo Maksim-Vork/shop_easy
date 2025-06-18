a05669d1541475567808be9e5c03b680ab297553
Maksim-Vork <maks41309@gmail.com>
2025-06-18 11:31:13 +0300

    Дописан UI настроек

--- chill_market/lib/features/profile/presentation/settings_screen.dart
+++ chill_market/lib/features/profile/presentation/settings_screen.dart
@@ -1,12 +1,20 @@
 import 'package:chill_market/core/Service/ThemeService/app_theme.dart';
+import 'package:chill_market/features/profile/domain/entity/settings.dart';
 import 'package:chill_market/features/profile/presentation/bloc/settings_bloc.dart';
+import 'package:chill_market/features/profile/presentation/bloc/settings_event.dart';
 import 'package:chill_market/features/profile/presentation/bloc/settings_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  String _selectedValue = 'Option 1';
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsBloc, SettingsState>(
@@ -127,10 +135,142 @@ class SettingsScreen extends StatelessWidget {
                   SizedBox(height: 8),
                   Container(
                     width: double.infinity,
-                    height: 158,
+                    height: 200,
                     decoration: BoxDecoration(
                       color: AppTheme.lightGray,
                       borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsetsGeometry.all(15),
+                      child: ListView.separated(
+                        itemBuilder: (context, index) {
+                          switch (index) {
+                            case 0:
+                              {
+                                return Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Темная тема',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Switch(
+                                      value: state.settings.isDart,
+                                      onChanged: (newValue) {
+                                        BlocProvider.of<SettingsBloc>(
+                                          context,
+                                        ).add(
+                                          EditSetting(
+                                            settings: Settings(
+                                              isDart: newValue,
+                                              language: state.settings.language,
+                                              currency: state.settings.currency,
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                );
+                              }
+                            case 1:
+                              {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 10,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Язык интерфейса',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+
+                                      Text(
+                                        state.settings.language,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+
+                            case 2:
+                              {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 10,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Валюта',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        state.settings.currency,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                            case 3:
+                              {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 10,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Изменить способ оплаты',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                          }
+                        },
+                        separatorBuilder:
+                            (BuildContext context, int index) => const Divider(
+                              height: 1,
+                              color: Color(0xFF7B7B7B),
+                            ),
+                        itemCount: 4,
+                      ),
                     ),
                   ),
                   SizedBox(height: 15),
@@ -138,10 +278,69 @@ class SettingsScreen extends StatelessWidget {
                   SizedBox(height: 8),
                   Container(
                     width: double.infinity,
-                    height: 158,
+                    height: 120,
                     decoration: BoxDecoration(
                       color: AppTheme.lightGray,
                       borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsetsGeometry.all(15),
+                      child: ListView.separated(
+                        itemBuilder: (context, index) {
+                          switch (index) {
+                            case 0:
+                              {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 10,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Написать в поддержку',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                            case 1:
+                              {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 10,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Мы в соц. сетях',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                          }
+                        },
+                        separatorBuilder:
+                            (BuildContext context, int index) => const Divider(
+                              height: 1,
+                              color: Color(0xFF7B7B7B),
+                            ),
+                        itemCount: 2,
+                      ),
                     ),
                   ),
                 ],
